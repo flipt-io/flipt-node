3deb0e1b72e7379328e5005be608ac83995a3429
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-16 14:32:21 +0000

    SDK regeneration

--- src/serialization/resources/evaluation/types/EvaluationResponse.ts
+++ src/serialization/resources/evaluation/types/EvaluationResponse.ts
@@ -11,31 +11,22 @@ export const EvaluationResponse: core.serialization.ObjectSchema<
     FliptApi.evaluation.EvaluationResponse
 > = core.serialization.object({
     type: core.serialization.lazy(async () => (await import("../../..")).evaluation.EvaluationResponseType),
-    booleanResponse: core.serialization.property(
-        "boolean_response",
-        core.serialization
-            .lazyObject(async () => (await import("../../..")).evaluation.BooleanEvaluationResponse)
-            .optional()
-    ),
-    variantResponse: core.serialization.property(
-        "variant_response",
-        core.serialization
-            .lazyObject(async () => (await import("../../..")).evaluation.VariantEvaluationResponse)
-            .optional()
-    ),
-    errorResponse: core.serialization.property(
-        "error_response",
-        core.serialization
-            .lazyObject(async () => (await import("../../..")).evaluation.ErrorEvaluationResponse)
-            .optional()
-    ),
+    booleanResponse: core.serialization
+        .lazyObject(async () => (await import("../../..")).evaluation.BooleanEvaluationResponse)
+        .optional(),
+    variantResponse: core.serialization
+        .lazyObject(async () => (await import("../../..")).evaluation.VariantEvaluationResponse)
+        .optional(),
+    errorResponse: core.serialization
+        .lazyObject(async () => (await import("../../..")).evaluation.ErrorEvaluationResponse)
+        .optional(),
 });
 
 export declare namespace EvaluationResponse {
     interface Raw {
         type: serializers.evaluation.EvaluationResponseType.Raw;
-        boolean_response?: serializers.evaluation.BooleanEvaluationResponse.Raw | null;
-        variant_response?: serializers.evaluation.VariantEvaluationResponse.Raw | null;
-        error_response?: serializers.evaluation.ErrorEvaluationResponse.Raw | null;
+        booleanResponse?: serializers.evaluation.BooleanEvaluationResponse.Raw | null;
+        variantResponse?: serializers.evaluation.VariantEvaluationResponse.Raw | null;
+        errorResponse?: serializers.evaluation.ErrorEvaluationResponse.Raw | null;
     }
 }
